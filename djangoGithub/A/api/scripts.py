940e339a23cada5ec96e95d1c9af0cc422c4e897
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-05 17:02:49 +0330

    fix bugs

--- djangoGithub/A/api/scripts.py
+++ djangoGithub/A/api/scripts.py
@@ -5,5 +5,6 @@ def get_data():
     url = f"https://api.github.com/users/Amir-Mohamad"
     r = requests.get(url)
     data = r.json()
-    data_list = [i for i in data['id']]
-    return data_list
+    for key, value in data.items():
+        return(key, value)
+    
