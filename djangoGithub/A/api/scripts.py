19bb6a6bec09902305392532f30c6995060d51a5
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-05 19:28:33 +0330

    Done

--- djangoGithub/A/api/scripts.py
+++ djangoGithub/A/api/scripts.py
@@ -3,7 +3,10 @@ import json
 
 
 def get_data(username):
+    # user account
     url = f"https://api.github.com/users/{username}"
+    # getting user account data from github api
     r = requests.get(url)
+    # json
     data_json = r.json()    
     return data_json
