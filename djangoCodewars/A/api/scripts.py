26e98be82222bfb7fbee6a5886e38c0a80f823d8
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-06 07:24:52 +0330

    update

--- djangoCodewars/A/api/scripts.py
+++ djangoCodewars/A/api/scripts.py
@@ -8,5 +8,5 @@ def get_data(username):
     # getting user account data from github api
     r = requests.get(url)
     # json
-    data_json = r.json()    
+    data_json = r.json()
     return data_json
